e0f79cbf40af1a5fd4a06649bf3cb46a7dc4788a
Vietor Liu <vietor.liu@gmail.com>
2012-07-21 08:33:25 +0800

    Rename JsonTool method name

--- src/org/vxwo/java/json/JsonTool.java
+++ src/org/vxwo/java/json/JsonTool.java
@@ -2,20 +2,20 @@ package org.vxwo.java.json;
 
 public final class JsonTool {
 
-	public static JsonValue read(String json) throws JsonException {
+	public static JsonValue txtToValue(String json) throws JsonException {
 		return new JsonParser(json).decode();
 	}
 
-	public static JsonValue readObject(Object obj) throws JsonException {
+	public static JsonValue objToValue(Object obj) throws JsonException {
 		return new JsonObjParser(obj).Decode();
 	}
 
-	public static String write(JsonValue obj) throws JsonException {
+	public static String valueToTxt(JsonValue obj) throws JsonException {
 		return new JsonSerializer(true, false).toJSON(obj);
 	}
 
 	@SuppressWarnings("rawtypes")
-	public static Object writeObject(Class type, JsonValue obj)
+	public static Object valueToObj(Class type, JsonValue obj)
 			throws JsonException {
 		return new JsonObjSerializer().ConvertToObject(type, obj);
 	}
@@ -23,10 +23,10 @@ public final class JsonTool {
 	@SuppressWarnings("rawtypes")
 	public static Object deserialize(Class type, String json)
 			throws JsonException {
-		return writeObject(type, readObject(json));
+		return valueToObj(type, txtToValue(json));
 	}
 
 	public static String serialize(Object obj) throws JsonException {
-		return write(readObject(obj));
+		return valueToTxt(objToValue(obj));
 	}
 }
